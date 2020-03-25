12f2da112770f5a6d643960b0dc10a6e5c4ffb4a
Kyle Talmage <ktalmage85@gmail.com>
2020-03-25 17:58:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do
+    enable :sessions
+    set :session_secret, "Pepper85!"
+  end
+  
+  get '/' do
+    erb :index
+  end
+  
+  post '/checkout' do
+    
+    erb :checkout
+  end
+  
 end
